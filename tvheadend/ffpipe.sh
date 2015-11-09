b787533e817a587178e92ac698cb6e5f27664683
pscherrer80 <pscherrer80@gmail.com>
2015-11-09 10:45:41 +0100

    Delete ffpipe.sh

--- tvheadend/ffpipe.sh
+++
@@ -1,6 +0,0 @@
-name=$1
-url=`echo $2 | cut -d'|' -f1`
-forward=`echo $2 | cut -d'|' -f2`
-forward=${forward/=/:}
-
-/usr/bin/ffmpeg -loglevel fatal -headers $forward$'\r\n' -i $url -vcodec copy -acodec copy -metadata service_provider=IPTV -metadata service_name=$name -f mpegts pipe:1
